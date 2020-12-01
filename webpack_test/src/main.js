1ae60e2037e88ae7302bc2632588c43f60958a68
Zenas.Fan <Zenas.Fan@homecreditcfc.cn>
2020-12-01 10:27:12 +0800

    feat: 入口文件

--- webpack_test/src/main.js
+++ webpack_test/src/main.js
@@ -1,9 +1,8 @@
-console.log(333333);
 // debugger;
 import Vue from "vue";
-console.log(Vue);
-// import app from "./app.vue";
+// console.log(Vue);
+import app from "./app.vue";
 
-// new Vue({
-//   render: (h) => h(app),
-// }).$mount("#app");
+new Vue({
+  render: (h) => h(app),
+}).$mount("#app");
